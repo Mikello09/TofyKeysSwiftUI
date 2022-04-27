0473377cf5bdbf5d0b69c410dd7de153d3090e3f
Mikel Lopez <mikelsalazarlopez@gmail.com>
2022-04-27 15:43:11 +0200

    Loading Lottie

---
+++ TofyKeys/Helper/LottieView.swift
@@ -0,0 +1,37 @@
+//
+//  LottieView.swift
+//  TofyKeys
+//
+//  Created by Mikel Lopez Salazar on 27/4/22.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode
+    
+    var animationView = AnimationView()
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {}
+}
--- TofyKeys/Views/Login/LoginView.swift
+++ TofyKeys/Views/Login/LoginView.swift
@@ -13,67 +13,84 @@ struct LoginView: View {
     
     @Environment(\.dismiss) var dismiss
     
+    // Loading
+    @State var showLoading: Bool = false
+    
+    // Tofy Login
     @State var email: String = ""
     @State var pass: String = ""
     
     var body: some View {
-        VStack {
-            // MARK: NAVIGATION
+        ZStack {
             VStack {
-                HStack {
-                    Spacer()
-                    Button (action: {
-                        dismiss()
-                    }, label: {
-                        imageButton(systemName: "xmark", color: .primaryColor)
-                    })
-                }
-            }.frame(height: 50)
-            
-            // MARK: LOG IN WITH TOFY
-            VStack(spacing: 0) {
-                HStack{
-                    Text(LocalizedStringKey("tofy_login"))
-                        .title()
-                    Spacer()
-                }
-                VStack(spacing: 0) {
-                    TextField(LocalizedStringKey("Email"), text: $email)
-                        .textFieldStyle(LoginTextFieldStyle())
-                    TextField(LocalizedStringKey("Password"), text: $pass)
-                        .textFieldStyle(LoginTextFieldStyle())
-                }.padding([.top, .bottom])
+                // MARK: NAVIGATION
                 VStack {
-                    Button (action: {
-                        print("hola")
-                    }, label: {PrincipalButtonText(LocalizedStringKey("enter"))}).buttonStyle(PrincipalButton())
+                    HStack {
+                        Spacer()
+                        Button (action: {
+                            dismiss()
+                        }, label: {
+                            imageButton(systemName: "xmark", color: .primaryColor)
+                        })
+                    }
+                }.frame(height: 50)
+                
+                // MARK: LOG IN WITH TOFY
+                VStack(spacing: 0) {
+                    HStack{
+                        Text(LocalizedStringKey("tofy_login"))
+                            .title()
+                        Spacer()
+                    }
+                    VStack(spacing: 0) {
+                        TextField(LocalizedStringKey("Email"), text: $email)
+                            .textFieldStyle(LoginTextFieldStyle())
+                        TextField(LocalizedStringKey("Password"), text: $pass)
+                            .textFieldStyle(LoginTextFieldStyle())
+                    }.padding([.top, .bottom])
+                    VStack {
+                        Button (action: {
+                            showLoading = true
+                        }, label: {PrincipalButtonText(LocalizedStringKey("enter"))}).buttonStyle(PrincipalButton())
 
+                    }
                 }
-            }
-            .padding()
-            .background(.white)
-            .cornerRadius(8)
-            .shadow(color: .gray, radius: 8, x: 0, y: 0)
-            .padding()
-            
-            // MARK: CONTINUE WITH APPLE
-            SignInWithAppleButton(.continue) { request in
-                print("")
-            } onCompletion: { result in
-                print("")
-            }
-            .padding()
-            .frame(height: 92)
+                .padding()
+                .background(.white)
+                .cornerRadius(8)
+                .shadow(color: .gray, radius: 8, x: 0, y: 0)
+                .padding()
+                
+                // MARK: CONTINUE WITH APPLE
+                SignInWithAppleButton(.continue) { request in
+                    print("")
+                } onCompletion: { result in
+                    print("")
+                }
+                .padding()
+                .frame(height: 92)
 
-            Spacer()
+                Spacer()
+                
+                // MARK: REGISTER
+                Button (action: {
+                    print("Register")
+                },label: {
+                    SecondaryButtonText(LocalizedStringKey("no_account"))
+                })
+                .buttonStyle(SecondaryButton())
+                
+            }
             
-            // MARK: REGISTER
-            Button (action: {
-                print("Register")
-            },label: {
-                SecondaryButtonText(LocalizedStringKey("no_account"))
-            })
-            .buttonStyle(SecondaryButton())
+            // MARK: LOADING
+            if showLoading {
+                VStack {
+                    LottieView(name: "loading", loopMode: .loop)
+                        .frame(width: 64, height: 64)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.whiteHighlighted)
+            }
         }
     }
 }
